fd14353cb06588d0f445bb743ee61306c2cb7c69
Mattia46 <mattia.assogna@gmail.com>
2015-11-26 16:57:01 +0000

    added 2 unit test: rating average

--- spec/feature/reviews_feature_spec.rb
+++ spec/feature/reviews_feature_spec.rb
@@ -37,4 +37,18 @@ feature 'reviewing' do
     click_button 'Leave Review'
     expect(page).to have_content('You have to log in firts')
   end
+
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
 end
