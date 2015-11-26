056187123133bbee652f8b6da8d722ea84513658
Mattia46 <mattia.assogna@gmail.com>
2015-11-26 18:13:06 +0000

    complete the rating average and added the stars rating

--- spec/reviews_helper_spec.rb
+++ spec/reviews_helper_spec.rb
@@ -6,4 +6,12 @@ describe ReviewsHelper, :type => :helper do
       expect(helper.star_rating('N/A')).to eq 'N/A'
     end
   end
+
+  it 'returns three black stars and two white stars for three' do
+    expect(helper.star_rating(3)).to eq '★★★☆☆'
+  end
+
+  it 'returns four black stars and one white star for 3.5' do
+    expect(helper.star_rating(3.5)).to eq '★★★★☆'
+  end
 end
