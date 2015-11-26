50d3d36c507e05c9bfeebbe6e30ad6554486b077
Mattia46 <mattia.assogna@gmail.com>
2015-11-26 18:57:54 +0000

    completed endorsements

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,9 @@
 class Review < ActiveRecord::Base
+
   validates :user_id, :uniqueness => { :scope => :restaurant_id}
+  validates :rating, inclusion: (1..5)
   belongs_to :restaurant
   belongs_to :user
-  validates :rating, inclusion: (1..5)
+  has_many :endorsements
 
 end
